5fc0cac7462efd15ccd36cd6dc76596ff8a53ec9
NataliaZvezdina <91384130+NataliaZvezdina@users.noreply.github.com>
2024-01-17 22:48:44 +0300

    Time: 467 ms (96.43%) | Memory: 54.8 MB (96.43%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.scala
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.scala
@@ -4,6 +4,14 @@ object Solution {
     def uniqueOccurrences(arr: Array[Int]): Boolean = {
         val mp = Map.empty[Int, Int].withDefaultValue(0)
         for (num <- arr) mp(num) += 1
-        mp.values.forall(fq => mp.values.count(fqCount => fq == fqCount) == 1)
+        val fq = mp.values.toSeq
+
+        def loop(fq: Seq[Int], acc: Set[Int] = Set[Int]()): Boolean = fq match {
+            case Nil => true
+            case head :: tail if acc.contains(head) => false
+            case head :: tail => loop(tail, acc + head)
+        }
+
+        loop(fq)
     }
 }
