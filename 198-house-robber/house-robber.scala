8e40579c15308cdbb00fd7d700ca7b19fbfa3560
NataliaZvezdina <91384130+NataliaZvezdina@users.noreply.github.com>
2024-01-21 20:43:15 +0300

    Time: 454 ms (58.06%) | Memory: 55 MB (38.71%) - LeetSync

--- 198-house-robber/house-robber.scala
+++ 198-house-robber/house-robber.scala
@@ -3,12 +3,11 @@ import scala.annotation._
 object Solution {
     def rob(nums: Array[Int]): Int = {
         @tailrec
-        def loop(idx: Int, prev: Int, curr: Int): Int = {
-            if (idx == nums.length) curr
-            else loop(idx + 1, curr, math.max(curr, prev + nums(idx)))
+        def loop(idx: Int = 0, twoBack: Int = 0, oneBack: Int = 0): Int = {
+            if (idx == nums.length) oneBack
+            else loop(idx + 1, oneBack, math.max(oneBack, twoBack + nums(idx)))
         }
 
-        if (nums.length == 1) nums(0)
-        else loop(2, nums(0), math.max(nums(0), nums(1)))
+        loop()
     }
 }
